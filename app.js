7dce202355a3d97cbd26591f1b2b21de83fbf060
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-07 21:24:56 +0600

    login route..

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Premium Bag Shop API!");
 });
+app.get("/login", (req, res) => {
+  res.send("Welcome to Premium Bag Shop API!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
