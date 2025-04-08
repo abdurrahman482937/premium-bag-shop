20be19486511ec317de8d20206635e14e1484817
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-08 20:57:40 +0600

    use

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const cookieParser = require("cookie-parser");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Premium Bag Shop API!");
