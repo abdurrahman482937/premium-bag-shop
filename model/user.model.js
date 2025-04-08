85aaebdcb372b7a57b1751c6a4a7cc9480c3a0e1
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-08 21:26:36 +0600

    full user schema..

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/mongo")
+mongoose.connect("mongodb://127.0.0.1:27017/mongo");
 
 const userSchema = mongoose.Schema({
-    
-})
+  fullname: String,
+  email: String,
+  password: String,
+  cart: {
+    type: Array,
+    default: [],
+  },
+  isAdmin: Boolean,
+  orders: {
+    type: Array,
+    default: [],
+  },
+  contact: Number,
+  picture: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
