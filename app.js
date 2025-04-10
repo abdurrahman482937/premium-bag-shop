7f3c3f5257831ecd0b6d183a149134c533655c8f
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-10 22:18:19 +0600

    app..

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
 
+const usersRouter = require("./routes/usersRouter");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const db = require("./config/mongooseConnection");
+
 const app = express();
 const PORT = 3000;
 
@@ -9,15 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Premium Bag Shop API!");
-});
-app.get("/login", (req, res) => {
-  res.send("Welcome to Premium Bag Shop API!");
-});
+app.use("/users", usersRouter);
+app.use("/owners", ownersRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
