f9dbd0a89f10be696931f4eac80b32e3c5ae93e1
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-12 21:25:21 +0600

    app

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
+const expressSession = require("express-session");
+const flash = require("flash");
+require("dotenv").config();
 
 const usersRouter = require("./routes/usersRouter");
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
+const indexRouter = require("./routes/index");
 
 const db = require("./config/mongooseConnection");
 
@@ -14,9 +18,18 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/owners", ownersRouter);
 app.use("/products", productsRouter);
