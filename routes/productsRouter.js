5090a575e1c72350cd8d54057c517eb7aca9f284
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-14 21:38:47 +0600

    product router..

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const upload = require("../config/multer-config");
+const productModel = require("../model/product.model");
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("hey");
+router.post("/create", upload.single("image"), async function (req, res) {
+  try {
+    const { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+    let product = await productModel.create({
+      image: req.file.buffer,
+      name,
+      price,
+      discount,
+      bgcolor,
+      panelcolor,
+      textcolor,
+    });
+    req.flash("success", "Product create successfully!");
+    res.redirect("/owners/admin");
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 module.exports = router;
