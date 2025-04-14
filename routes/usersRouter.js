f826e01752c632affc59e487e08196b4e5f7395a
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-14 21:38:56 +0600

    user router..

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controller/authController");
 const router = express.Router();
 
 router.get("/", function (req, res) {
   res.send("hey");
 });
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
 
 module.exports = router;
